07cce83ff125ab56e7ea4304505ba98759c9269b
anandteertha <anandteertha.ramesh18@siesgst.ac.in>
2020-11-05 10:57:50 +0530

    server

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static('/'));
 var db_config = {
 	host     : 'localhost',
 	user     : 'root',
-	password : 'anantdf123099#qcvbnAS!!++#',
+	password : 'hello world',
 	database : 'wdl'
 };
 var connection = mysql.createPool(db_config);
